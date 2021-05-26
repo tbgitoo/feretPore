7588ed1f10afc23660d4d8dc48d97279f711596d
tbgitoo <thomas.braschler@gmail.com>
2021-05-26 23:26:41 +0200

    Add FeretPore to source

---
+++ feretPore/src/FeretPore.java
@@ -0,0 +1,16 @@
+import ij.gui.DialogListener;
+import ij.plugin.filter.PlugInFilter;
+import ij.ImagePlus;
+
+public class FeretPore {
+
+// image to work on
+	protected ImagePlus imp;
+
+	// Constructor, initialize the choices and the initial choice
+	public FeretPore()
+	{
+
+	}
+
+}
